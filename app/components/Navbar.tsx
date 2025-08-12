8400e45fc7212344f57870458ba580b713a4b5bd
raykaris <ryanmuthoni050@gmail.com>
2025-08-12 09:09:39 +0300

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router"
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <Link to={"/"}>
+        <p className="text-2xl font-bold text-gradient">ResumeGen</p>
+      </Link>
+      <Link to={"/upload"} className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </nav>
   )
 }
 
