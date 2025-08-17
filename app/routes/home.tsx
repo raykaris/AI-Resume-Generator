fceacc2ffafa015d429465fe972b50b2017c03b7
raykaris <ryanmuthoni050@gmail.com>
2025-08-17 08:21:20 +0200

    update

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,22 +13,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powerd feedback.</h2>
-      </div>
-    </section>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
-   {resumes.length > 0 && (
-    <div className="resumes-section">
-      {resumes.map((resume) => (
-        <ResumeCard key={resume.id} resume={resume}  />
-      ))}
-    </div>
-   )}
-  </main>
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powerd feedback.</h2>
+        </div>
+
+        {/* Resumes sample */}
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
