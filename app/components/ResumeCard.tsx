33373092e725bed14a46112f9aeb3d719be07b3b
raykaris <ryanmuthoni050@gmail.com>
2025-08-17 08:15:36 +0200

    update

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 import ScoreCircle from './ScoreCircle';
 
-const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback} } : { resume: Resume }) => {
+const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback, imagePath} } : { resume: Resume }) => {
   return (
     <Link
       to={`/resume/${id}`}
@@ -14,6 +14,11 @@ const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback} } : { resume
         </div>
         <div className="flex-shrink-0">
           <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      <div className='gradient-border animate-in fade-in duration-1000'>
+        <div className='w-full h-full'>
+          <img src={imagePath} alt="resume" className='w-full h-[350px] max-sm:h-[200px] object-cover object-top' />
         </div>
       </div>
     </Link>
